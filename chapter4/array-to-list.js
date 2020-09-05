7f50e27a63d740bbb3b71a6be2203ca77dd7df6d
Guillaume Wells <wellsguillaume@gmail.com>
2020-09-05 15:45:10 +0200

    array_to_list

--- chapter4/array-to-list.js
+++ chapter4/array-to-list.js
@@ -11,6 +11,43 @@ function arrayToList(array) {
     return lastNode
 }
 
+function listToArray(list) {
+    let array = [];
+    while(list){
+        array.push(list.value) 
+        list = list.next
+    }
+    return array;
+}
+
+function prepend(list, el) {
+    list = arrayToList(reverseArrayInPlace(listToArray(list)))
+    let lastNode = null
+    while(list) {
+        let node = {value: list.value, next: lastNode}
+        lastNode = node
+        list = list.next
+    }
+    let node = { value: el.value, next: lastNode}
+    return node
+}
+
+function getNth(list, nth) {
+    for(i = 0; i != nth && list; i++) {
+        list = list.next
+    }
+    return list ? list.value : undefined
+}
+
+function getNthRecursive(list, nth) {
+    if (nth != 0 && list)
+        return getNthRecursive(list.next, nth - 1)
+    else if (list)
+        return list.value
+    else
+        return undefined
+}
+
 function reverseArrayInPlace(array) {
     let middle = Math.floor(array.length / 2);
     let bkp;
@@ -23,4 +60,8 @@ function reverseArrayInPlace(array) {
     return array;
 }
 
-console.log(util.inspect(arrayToList([1,2,3,4,5,6,7,8,9,10]), {showHidden: false, depth: null}))
+//console.log(util.inspect(arrayToList([1,2,3,4,5,6,7,8,9,10]), {showHidden: false, depth: null}))
+//console.log(listToArray(arrayToList([1,2,3,4,5,6,7,8,9,10])))
+///console.log(util.inspect(prepend(arrayToList([1,2,3,4,5,6,7,8,9,10]), {value: 6, next: null}), {showHidden: false, depth: null}));
+//console.log(getNth(arrayToList([1,2,3,4,5,6,7,8,9,10]), -1))
+//console.log(getNthRecursive(arrayToList([1,2,3,4,5,6,7,8,9,10]), -1))
